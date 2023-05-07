8fb277dbf34ae526c54ffb88b2d3445403924ded
MrStark1234 <117528760+MrStark1234@users.noreply.github.com>
2023-05-07 19:19:00 +0530

    trying to change the value of cors

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ const server = app.listen(PORT, () =>
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*" || "http://localhost:3000",
   },
 });
 io.on("connection", (socket) => {
